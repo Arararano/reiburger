a377c7c51b5e3b0eb72ab034303e5d4433b7dd4e
Arararano <eagleraath@gmail.com>
2023-07-06 16:43:25 -0400

    using example

--- src/main.rs
+++ src/main.rs
@@ -44,6 +44,38 @@ impl EventHandler for Bot {
             }
         }
 
+        if msg.content == "!example" {
+            // The create message builder allows you to easily create embeds and messages
+            // using a builder syntax.
+            // This example will create a message that says "Hello, World!", with an embed that has
+            // a title, description, an image, three fields, and a footer.
+            let msg = msg
+                .channel_id
+                .send_message(&ctx.http, |m| {
+                    m.content("Hello, World!")
+                        .embed(|e| {
+                            e.title("This is a title")
+                                .description("This is a description")
+                                .image("attachment://ferris_eyes.png")
+                                .fields(vec![
+                                    ("This is the first field", "This is a field body", true),
+                                    ("This is the second field", "Both fields are inline", true),
+                                ])
+                                .field("This is the third field", "This is not an inline field", false)
+                                .footer(|f| f.text("This is a footer"))
+                                // Add a timestamp for the current time
+                                // This also accepts a rfc3339 Timestamp
+                                .timestamp(Timestamp::now())
+                        })
+                        .add_file("./ferris_eyes.png")
+                })
+                .await;
+
+            if let Err(why) = msg {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+
 
     }
 
